392c3dfac86474f6d0505f0d3f7cb671599f1784
Patrick Brandt <pbrandt@nrao.edu>
2009-12-23 10:59:32 -0500

    Refactored xstr2float and float2xstr.

--- src/utility.py
+++ src/utility.py
@@ -20,55 +20,26 @@ def generate_mask(n):
     """Generates a bitmask of all 1s of the specified length."""
     return (1 << n) - 1
 
-def xstr2float(xstr, frac_bits=0, sign_bit=None, radix=16):
+def xstr2float(xstr, frac_bits=0, sign_bit=32, radix=16):
     """Converts a numeric string from xilinx fixed point to floating point.
 
     'xstr' is expected to be a hex string, but can be any base as long
     as the proper radix is provided. All xstr values must be in string
     format, however.
     """
-    mask = generate_mask(frac_bits)
-    upper = int(xstr, radix) >> frac_bits
-    lower = int(xstr, radix) & mask
-    if sign_bit:
-        temp = sign_bit - frac_bits - 1
-        sign = upper & (1 << temp)
-        upper = (upper & generate_mask(temp)) - sign
-    for i in range(1, frac_bits + 1):
-        if (lower >> (frac_bits - i)) & 1:
-            upper += 1.0 / (1 << i)
-    return float(upper)
+    n = int(xstr, radix)
+    if sign_bit > 0 and n & (1 << (sign_bit - 1)):
+        n -= (1 << sign_bit)
+    return float(n) / float(1 << frac_bits)
 
-def float2xstr(num, frac_bits=0, sign_bit=None):
+def float2xstr(num, frac_bits=0, sign_bit=32):
     """Converts a floating point number to a xilinx fixed point hex string.
 
     'num' can be an integer or floating point, positive or negative.
     Unlike xstr2float, this function does not (currently) allow a radix
     to be specified but instead always converts from base ten to hex.
     """
-    upper = int(num)
-    lower = num - upper
-    if sign_bit and (lower < 0 or upper < 0):
-        upper += (1 << (sign_bit - frac_bits - 1))
-        # Turn negative fractions into their positive complements of 1
-        lower += 1 - int(lower + 1)
-        # Correct for the above complementing in the whole number portion
-        if (frac_bits > 0) and (lower > 0): upper -= 1
-    # Quantize the fractional part of the number
-    for i in range(1, frac_bits + 1):
-        upper <<= 1
-        test = 1.0 / (1 << i)
-        if lower >= test:
-            upper |= 1
-            lower -= test
-    # Apply sign bit if the number is negative
-    if sign_bit and num < 0:
-        sign_mask = 1 << (sign_bit - 1)
-        upper |= sign_mask
-    upper = hex(upper)
-    # Python 2.4+ hack (hex() prepends negative sign)
-    if upper.startswith('-'): upper = upper[1:]
-    # Some versions append 'L' for long-size values
-    if upper.endswith('L'): upper = upper[:-1]
-    # Slice off the '0x' portion of the hex string
-    return upper[2:]
+    n = num * (1 << frac_bits)
+    if sign_bit > 0 and num < 0:
+        n += (1 << sign_bit)
+    return '%x' % int(n)
