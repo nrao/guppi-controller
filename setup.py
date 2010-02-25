425cdcd69aa0852553ee94c8d7c4eec3c22e69a1
Ron DuPlain <rduplain@nrao.edu>
2010-02-25 12:24:18 -0500

    Working gpu supervisord + launch script.

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
   , package_dir = {'guppi' : 'src', 'guppi.scripts': 'src/scripts'}
   # , packages    = ['cicada', 'guppi']
   # , package_dir = {'cicada': 'src', 'guppi': path...})
-  , scripts = ['scripts/guppi', 'scripts/guppi_server', 'scripts/run_guppi_server']
+  , scripts = ['scripts/guppi', 'scripts/guppi_server', 'scripts/run_guppi_server', 'scripts/run_guppi_server_gpu']
   , maintainer = "NRAO"
   , maintainer_email = "rduplain@nrao.edu"
   # , url = ""
