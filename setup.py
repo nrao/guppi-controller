39257be24c986932f95971eeb42ea340e55fb34e
Ron DuPlain <ron.duplain@gmail.com>
2010-10-02 13:59:56 -0400

    Added custom utility scripts (written for BEE2).

--- setup.py
+++ setup.py
@@ -13,7 +13,9 @@ setup(
   , version     = '2010.09.30' # TODO: need a means to automatically update.
   , packages    = ['guppi', 'guppi.scripts']
   , package_dir = {'guppi' : 'src', 'guppi.scripts': 'src/scripts'}
-  , scripts = ['scripts/guppi', 'scripts/guppi_server', 'scripts/run_guppi_server', 'scripts/run_guppi_server_gpu']
+  , scripts = ['scripts/guppi', 'scripts/guppi_server', 'scripts/run_guppi_server', 'scripts/run_guppi_server_gpu',
+               'scripts/clr_tengbe', 'scripts/clr_tengbe.txt', 'scripts/findbof', 'scripts/killbof', 'scripts/set_tengbe', 'scripts/set_tengbe.txt',
+               ]
   , install_requires = [
         # 'guppi_daq', # not in pypi yet
         # 'soaplib', # currently uses 0.7.2 from Optio, which is out-of-date!
